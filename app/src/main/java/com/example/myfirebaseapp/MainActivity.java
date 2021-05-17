274de76e04ddd3bc7f9b2e5fcbe25d644d2464ee
Maritza <marysg98@yahoo.com>
2021-05-17 12:53:51 -0500

    Metodo de valiaciones de datos, metodo para limpar cajas, Agregue firedatabase en el gradle

--- app/src/main/java/com/example/myfirebaseapp/MainActivity.java
+++ app/src/main/java/com/example/myfirebaseapp/MainActivity.java
@@ -8,11 +8,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity
 {
-    private EditText nombrePersona, apellidoPersona, correoPersona, contraseñaPersona;
+    private EditText nombrePersona, apellidoPersona, correoPersona, contraseniaPersona;
     private ListView listVPersonas;
+
+    FirebaseDatabase firebaseDatabase; //Agregamos firedatabase
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -23,7 +30,7 @@ public class MainActivity extends AppCompatActivity
         nombrePersona = (EditText) findViewById(R.id.NombrePersona);
         apellidoPersona = (EditText) findViewById(R.id.ApellidosPersona);
         correoPersona = (EditText) findViewById(R.id.CorreoPersona);
-        contraseñaPersona = (EditText) findViewById(R.id.ContraseñaPersona);
+        contraseniaPersona = (EditText) findViewById(R.id.ContraseñaPersona);
 
         listVPersonas = (ListView) findViewById(R.id.lv_ListaPersonas);
     }
@@ -38,6 +45,67 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item)
     {
+        String nombre = nombrePersona.getText().toString();
+        String apellidos = apellidoPersona.getText().toString();
+        String correo = correoPersona.getText().toString();
+        String contrasenia = contraseniaPersona.getText().toString();
+        switch(item.getItemId())
+        {
+            case (R.id.icon_add):
+                if(nombre.equals("") ||apellidos.equals("") || correo.equals("") || contrasenia.equals(""))  //Agreagamos metodo de validaciones
+                {
+                    validacion();
+                    break;
+                }
+                else
+                {
+                    Toast.makeText(this, "Agregado", Toast.LENGTH_SHORT).show();
+                    limpiarCajas();   //Agregamos metodo para limpiar cajas, una vez agregado los datos se limpian para volver agregar mas datos
+                    break;
+                }
+        }
+
         return true;
+    }
+
+    private void limpiarCajas() //cambia el texto por texto en blanco
+    {
+        nombrePersona.setText("");
+        apellidoPersona.setText("");
+        correoPersona.setText("");
+        contraseniaPersona.setText("");
+    }
+
+    private void validacion()
+    {
+        String nombre = nombrePersona.getText().toString();
+        String apellidos = apellidoPersona.getText().toString();
+        String correo = correoPersona.getText().toString();
+        String contrasenia = contraseniaPersona.getText().toString();
+        if (nombre.equals(""))
+        {
+            nombrePersona.setError("Requerido");
+        }
+        else
+        {
+            if(apellidos.equals(""))
+            {
+                apellidoPersona.setError("Requerido");
+            }
+            else
+            {
+                if(correo.equals(""))
+                {
+                    correoPersona.setError("Requerido");
+                }
+                else
+                {
+                    if (contrasenia.equals(""))
+                    {
+                        contraseniaPersona.setError("Requerido");
+                    }
+                }
+            }
+        }
     }
 }
